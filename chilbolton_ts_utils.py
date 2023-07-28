4faf1873beb2715a97a4bcadd4b0767b25f7bf7b
longlostjames <chris.walden@ncas.ac.uk>
2023-07-28 12:20:31 +0100

    Pulse mode stuff

--- chilbolton_ts_utils.py
+++ chilbolton_ts_utils.py
@@ -1781,6 +1781,14 @@ def convert_galileo_ts_l0a2l1(infile,outfile,dBZ_offset,range_offset,data_versio
     varout.flag_meanings = 'not_used good_data probably_good_data bad_data data_in_blind_range no_qc_performed';
     varout[:] = 2;
 
+    pulse_mode = DSin['pulse_mode'];
+    varout = DSout.createVariable('pulse_mode','i4',('time'),zlib=True);
+    varout.long_name = 'Pulse mode';
+    varout.units = '1';
+    varout.comment = 'Mode for pulse transmission for each ray.  1 = H, 2 = V, 3 = HV (LDR mode), 4 = HH, 5 = VV, 6 = HV-VH, 7 = HV-HV'
+    varout[:-1] = pulse_mode[1:]; # Corrects offset of pulse_mode in NetCDF input file
+    varout[-1] = pulse_mode[-2];
+
     blind_range = np.arange(14);
     DSout['qc_flag'][:,:,blind_range] = 4;
 
